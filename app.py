4dc040dba5659b872bf7c99e56ca62a301d7151e
Sebastian Pandu W <89185866+babastian18@users.noreply.github.com>
2023-04-12 09:48:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    result = None
+    results = []
     if request.method == 'POST':
-        input_text = request.form['inputText']
-        result = len(input_text.split())
-    return render_template('index.html', result=result)
+        input_number = int(request.form['inputNumber'])
+        for i in range(input_number):
+            result = fibonacci(i)
+            results.append(result)
+    return render_template('index.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
